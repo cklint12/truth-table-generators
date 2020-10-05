fa23f383626ed491d6b69ed4c46f186b4170f5f1
cklint12 <cklint12@vt.edu>
2020-10-05 10:52:05 -0600

    Truth Table Generator for coin flips.

---
+++ ttg-coin-flips.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import itertools 
+
+num_flips = input("Enter number of flips: ") 
+table  = itertools.product('HT', repeat = num_flips)
+
+#Generate the table
+for outcome in table:
+    print(outcome)
+
