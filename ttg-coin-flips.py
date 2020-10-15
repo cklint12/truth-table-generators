e06a8166236d9f6776e1ae454b6f7cffc1a8ff4e
kciriaco <cklint12@vt.edu>
2020-10-15 16:46:46 -0600

    Fixed Comment

--- ttg-coin-flips.py
+++ ttg-coin-flips.py
@@ -5,7 +5,7 @@ import itertools
 num_flips = input("Enter number of flips: ") 
 table  = itertools.product('HT', repeat = num_flips)
 
-#Generate the table
+# Generate the table
 for outcome in table:
     print(outcome)
 
